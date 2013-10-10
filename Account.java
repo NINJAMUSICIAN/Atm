70c2c2005f7c1a331d004900b250695d3b563164
josh sparks <j80mar4@gmail.com>
2013-10-10 08:50:00 -0600

    added the ability to make an initial balance, withdraw and deposit

--- Account.java
+++ Account.java
@@ -5,17 +5,35 @@ public class Account{
 		private double balance;
 
 
-	public Account(int pin, String name, double balance){
+	public Account(int pin, String name){
 
 		this.pin = pin;
 		this.name = name;
-		this.balance = balance;
 
 	}
 
+	public void initialBalance(double money){
+		balance = money;
+	}
 
-	public static void main(String[] args) {
-		
+	public void withdraw(){
+		Scanner liger = new Scanner(System.in);
+
+		System.out.println("how much money would you like to withdraw?");
+
+		int amount = liger.nextDouble();
+
+		balance -= amount;
+	}
+
+	public void deposit(){
+		Scanner seuss = new Scanner(System.in);
+
+		System.out.println("how much would you like to deposit?");
+
+		int amount = seuss.nextDouble();
+
+		balance += amount;
 	}
 
 }
