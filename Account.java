49b1c0a8ac398688e6b5f54b8d933ffcb228f3a6
josh sparks <j80mar4@gmail.com>
2013-10-10 08:42:30 -0600

    began making account.java

---
+++ Account.java
@@ -0,0 +1,21 @@
+public class Account{
+
+		private int pin;
+		private String name;
+		private double balance;
+
+
+	public Account(int pin, String name, double balance){
+
+		this.pin = pin;
+		this.name = name;
+		this.balance = balance;
+
+	}
+
+
+	public static void main(String[] args) {
+		
+	}
+
+}
